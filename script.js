b99d4b0bc37b3b474e2eaaf9aa71510c84385add
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-06-17 00:43:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 function compute() {
-    p = document.getElementById("principal").value;
+    amount = document.getElementById("principal").value;
+    percent = document.getElementById("percentage").value;
 
 }
