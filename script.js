a2d9bb96c8b2a041da6329e5e48a9414f2aba68b
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-07-02 21:56:52 +0300

    Function calls are working

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 "use strict";
 
+function rate_refresh() {
+    alert("rate_refresh");
+    percent = document.getElementById("rate").value;
+    document.getElementById("percentage").innerText = string(percent);
+    return percent;
+}
+
 function compute() {
+    alert("compute");
     amount = document.getElementById("principal").value;
-    percent = document.getElementById("percentage").value;
+    percent = document.getElementById("rate").value;
     years_n = document.getElementById("years").value;
     res = (amount * percent / 100) * years_n;
-    alert(111);
     document.getElementById("result").innerText = string(res);
+    return res;
 }
