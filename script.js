c6f07f2481613c9a2baeeef125502a94b8bb8f5d
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-07-04 23:47:42 +0300

    Error catching implemented

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 function rate_refresh() {
     let percent = document.getElementById("rate").value;
     document.getElementById("percentage").innerHTML = percent + "%";
-    return percent;
+    return 0;
 }
 
 function compute() {
     let amount = document.getElementById("principal").value;
     let percent = document.getElementById("rate").value;
     let years_n = document.getElementById("years").value;
+    if (!(amount > 0)) {
+        alert("Amount have to be a positive number!");
+    }
     let currentTime = new Date();
     let cur_year = currentTime.getFullYear();
     let end_year = cur_year + Number(years_n);
@@ -18,5 +21,5 @@ function compute() {
         "at an interest rate of <span class=&quot;output&quot;>" + percent + "</span>%<br>" +
         "You will receive an amount of <span class=&quot;output&quot;>" + res + "</span><br>" +
         "in the year <span class=&quot;output&quot;>" + end_year + " </span><br>";
-    return res;
+    return 0;
 }
