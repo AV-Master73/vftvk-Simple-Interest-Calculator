be6d6662774bc2c12e83da468cfdc44e3b205d06
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-07-04 21:50:25 +0300

    Almost done

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function compute() {
     let percent = document.getElementById("rate").value;
     let years_n = document.getElementById("years").value;
     let res = (amount * percent / 100) * years_n;
-    alert(res);
     document.getElementById("result").innerHTML = "Interest : " + res;
     return res;
 }
