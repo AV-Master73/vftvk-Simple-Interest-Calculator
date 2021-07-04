42bc8cfce112ff7004a780263f27cdf9fa3e2cf5
AV-Master73 <85580482+AV-Master73@users.noreply.github.com>
2021-07-04 23:08:58 +0300

    Output is OK
    Yellow background for digits needed

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ function compute() {
     let amount = document.getElementById("principal").value;
     let percent = document.getElementById("rate").value;
     let years_n = document.getElementById("years").value;
+    let currentTime = new Date();
+    let cur_year = currentTime.getFullYear();
+    let end_year = cur_year + Number(years_n);
     let res = (amount * percent / 100) * years_n;
-    document.getElementById("result").innerHTML = "Interest : " + res;
+    document.getElementById("result").innerHTML = "If you deposit " + amount + "<br>" +
+        "at an interest rate of " + percent + "%<br>" +
+        "You will receive an amount of " + res + "<br>" +
+        "in the year " + end_year + "<br>";
     return res;
 }
